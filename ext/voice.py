4deaf07cea8d9782c7f640182f57664d21d712ab
Nicholas Hammond <nhammond129@gmail.com>
2016-10-10 22:01:33 -0500

    moar voice stuff

--- ext/voice.py
+++ ext/voice.py
@@ -14,7 +14,12 @@ async def digest(message,bot):
         tokens[0]=tokens[0][1:]
         print(tokens)
         if isMatch(tokens[0],"voice"):
-            if isMatch(tokens[1],"jc"):
+            if isMatch(tokens[1],"help"):
+                await bot.send_message(
+                        message.channel,
+                        "jc rr2 mycube pain gems pain bustin kurisutina mybrand"
+                        )
+            elif isMatch(tokens[1],"jc"):
                 if bot.is_voice_connected(
                         message.channel.server
                         ):
@@ -28,6 +33,38 @@ async def digest(message,bot):
                         )
                 player = await vc.create_ytdl_player(
                         "https://www.youtube.com/watch?v=ggKeB9OOkRY"
+                        )
+                player.start()
+            elif isMatch(tokens[1],"mybrand"):
+                if bot.is_voice_connected(
+                        message.channel.server
+                        ):
+                    pass
+                else:
+                    await bot.join_voice_channel(
+                        message.author.voice_channel
+                        )
+                vc = bot.voice_client_in(
+                        message.channel.server
+                        )
+                player = await vc.create_ytdl_player(
+                        "https://www.youtube.com/watch?v=V-fRuoMIfpw"
+                        )
+                player.start()
+            elif isMatch(tokens[1],"kurisutina"):
+                if bot.is_voice_connected(
+                        message.channel.server
+                        ):
+                    pass
+                else:
+                    await bot.join_voice_channel(
+                        message.author.voice_channel
+                        )
+                vc = bot.voice_client_in(
+                        message.channel.server
+                        )
+                player = await vc.create_ytdl_player(
+                        "https://www.youtube.com/watch?v=a-GqSWsISVs"
                         )
                 player.start()
             elif isMatch(tokens[1],"rr2"):
@@ -46,7 +83,7 @@ async def digest(message,bot):
                         "https://www.youtube.com/watch?v=0NlfQdH5MSg"
                         )
                 player.start()
-            elif isMatch(tokens[1],"purpose"):
+            elif isMatch(tokens[1],"mycube"):
                 if bot.is_voice_connected(
                         message.channel.server
                         ):
@@ -59,7 +96,23 @@ async def digest(message,bot):
                         message.channel.server
                         )
                 player = await vc.create_ytdl_player(
-                        "https://www.youtube.com/watch?v=wqzLoXjFT34"
+                        "https://www.youtube.com/watch?v=Uf-5l2nebRE"
+                        )
+                player.start()
+            elif isMatch(tokens[1],"pain"):
+                if bot.is_voice_connected(
+                        message.channel.server
+                        ):
+                    pass
+                else:
+                    await bot.join_voice_channel(
+                        message.author.voice_channel
+                        )
+                vc = bot.voice_client_in(
+                        message.channel.server
+                        )
+                player = await vc.create_ytdl_player(
+                        "https://www.youtube.com/watch?v=KSI03_RPVTU"
                         )
                 player.start()
             elif isMatch(tokens[1],"bustin"):
@@ -78,7 +131,7 @@ async def digest(message,bot):
                         "https://www.youtube.com/watch?v=ybcS7CP4Om0"
                         )
                 player.start()
-            elif isMatch(tokens[1],"familiar"):
+            elif isMatch(tokens[1],"gems"):
                 if bot.is_voice_connected(
                         message.channel.server
                         ):
@@ -91,7 +144,7 @@ async def digest(message,bot):
                         message.channel.server
                         )
                 player = await vc.create_ytdl_player(
-                        "https://www.youtube.com/watch?v=H45jqrhcsOY"
+                        "https://www.youtube.com/watch?v=34f6RZDDBI4"
                         )
                 player.start()
             else:
@@ -100,5 +153,3 @@ async def digest(message,bot):
             pass
     else:
         pass
-
-
